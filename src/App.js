de4bac1b8dd9d1f2f16d493b18214ba10f17011d
Daniel Hausen <danielhausen@Daniels-MacBook-Pro.local>
2024-01-19 15:04:03 -0500

    removed vertical timeline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
+
 
 function App() {
   return (
